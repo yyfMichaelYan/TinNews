61f65c8eac8dc59b0fc668b9da4b6f278c17d4d0
yyfMichaelYan <yyfmichaelyan@gmail.com>
2020-09-06 11:45:53 -0500

    added view model and repository

--- app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
+++ app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -4,11 +4,13 @@ import android.app.Application;
 
 import com.ashokvarma.gander.Gander;
 import com.ashokvarma.gander.imdb.GanderIMDB;
+import com.facebook.stetho.Stetho;
 
 public class TinNewsApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Gander.setGanderStorage(GanderIMDB.getInstance());
+        Stetho.initializeWithDefaults(this);
     }
 }
--- app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
+++ app/src/main/java/com/laioffer/tinnews/ui/search/SearchFragment.java
@@ -2,28 +2,48 @@ package com.laioffer.tinnews.ui.search;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.laioffer.tinnews.R;
+import com.laioffer.tinnews.repository.NewsRepository;
+import com.laioffer.tinnews.repository.NewsViewModelFactory;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
+    private SearchViewModel viewModel;
 
     public SearchFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NewsRepository repository = new NewsRepository(getContext());
+        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository)).get(SearchViewModel.class);
+        viewModel.setSearchInput("Covid-19");
+        viewModel.searchNews().observe(getViewLifecycleOwner(), newsResponse -> {
+            if (newsResponse != null) {
+                Log.d("SearchFragment", newsResponse.toString());
+            }
+        });
     }
 }
